39d645fd37fa226cafdccc031c798bdebeaf92bb
Alex_Nett <AN@gmail.com>
2018-12-04 10:03:24 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void main(){
+	for(int i = 1; i < 11; i++){
+		cout << i + "\n";
+	}
+	system("pause");
+}
+
+//Alexander Nettenbreijers. Verifica.
+//4-12-2018
+//Test
